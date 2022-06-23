044f60189f098526d483ed604afc609e74b10953
Shaheer <shaheer.myhome@gmail.com>
2022-06-23 23:02:07 +0530

    First Commit

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 - Status checker is a discord bot that will check your sites statuses
 
-#### Instalation
+#### Installation
 
-1. Didn't finish it yet, I will push src once completed.
+1. Download this code to your machine or vm and navigate to your directory.
+2. Run `npm install`.
+3. edit the config.json by adding your sites (you can add upto unlimited sites).
+4. Add your channel.
+5. Add your interval (in milliseconds).
+6. Customize the message and stuff, this is just a basic template you can do whatever you want with it!
+7. Add an env variable called "token" with its value to be your discord bot token.
+8. Have fun!
---
+++ index.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const {channel, sites, interval}  = require('./config.json');
+const {Client} = require('discord.js');
+const fetch = require('node-fetch');
+const { token } = process.env;
+const client = new Client({
+    intents: ["GUILDS",
+    
+    "GUILD_MESSAGES",
+    "GUILD_MESSAGE_REACTIONS",
+    "GUILD_MESSAGE_TYPING",
+    "DIRECT_MESSAGES",
+    "DIRECT_MESSAGE_REACTIONS",
+    "DIRECT_MESSAGE_TYPING"],
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+    restTimeOffset: 0,
+    allowedMentions: {
+        parse: ['users']
+    }
+})
+
+
+
+client.on('ready', () => {
+    const channel_exist = client.channels.cache.get(channel)
+    if(!channel_exist){
+        console.error('Channel not found please reconfigure config.json, process exiting...')
+      return client.destroy() && process.exit(1)
+    }
+    const channelname = channel_exist.name
+console.log("Started Monitoring " + sites.length + " sites" + " on channel: #" + channelname);
+  
+    client.user.setActivity(`Monitoring ${sites.length} sites`, { type: 'PLAYING' });
+
+})
+
+
+client.on('ready', () => {
+    setInterval(async () => {
+    sites.forEach(async site => {
+        const status = await fetch(site.url, { method: 'HEAD' 
+    })
+   if(status.status !== 200){
+    return client.channels.cache.get(channel).send(`:x: ${site.name} is down. Status code: ${status.status}`)
+   } else {
+ 
+    return client.channels.cache.get(channel).send(`:white_check_mark: ${site.name} is up and running status code: ${status.status}`)
+
+   }
+    })
+}, interval) 
+
+
+})
+
+
+
+
+client.login(token);
